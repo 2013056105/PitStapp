6025590b6c3ad5d4c206d48e46452b0d6210c68b
Jonas Aguilar <2013056093@ust.edu.ph>
2017-02-22 09:53:09 +0800

    added layouts

--- app/src/main/java/com/example/alec/pitstapp/MainActivity.java
+++ app/src/main/java/com/example/alec/pitstapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alec.pitstapp;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.alec.pitstapp.Fragments.MapFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        moveToMapFragment();
+    }
+
+    public void moveToMapFragment() {
+        MapFragment mapFragment = new MapFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(mapFragment, "mapFragment")
+                .replace(R.id.fragment_container, mapFragment)
+                .commit();
     }
 }
