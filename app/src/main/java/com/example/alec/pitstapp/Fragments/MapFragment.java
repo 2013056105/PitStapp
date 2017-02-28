ba42104634dcad5255bf439ec9092636e7fc742c
Ryan Madarang <madarang.ryan@gmail.com>
2017-02-28 23:04:45 +0800

    updates to mapfragment, added mapkey, splash, etc.

--- app/src/main/java/com/example/alec/pitstapp/Fragments/MapFragment.java
+++ app/src/main/java/com/example/alec/pitstapp/Fragments/MapFragment.java
@@ -8,11 +8,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.alec.pitstapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback{
+
+    MapView mMapView;
+    GoogleMap mGoogleMap;
 
 
     public MapFragment() {
@@ -23,8 +32,53 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mMapView = (MapView) v.findViewById(R.id.mapview);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this); //this is important
+
+        return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng ustPosition = new LatLng(14.609676, 120.989658);
+        mGoogleMap = googleMap;
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        mGoogleMap.addMarker(new MarkerOptions().position(ustPosition));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ustPosition, 10));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 
 }
