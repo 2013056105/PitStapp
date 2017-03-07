a1525c5750596bbd332112a05ee07d4bbf4519b5
Ryan Madarang <madarang.ryan@gmail.com>
2017-03-08 07:29:23 +0800

    Updates but still broken

--- app/src/main/java/com/example/alec/pitstapp/Fragments/MapFragment.java
+++ app/src/main/java/com/example/alec/pitstapp/Fragments/MapFragment.java
@@ -1,13 +1,31 @@
 package com.example.alec.pitstapp.Fragments;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.alec.pitstapp.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -15,13 +33,31 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment implements OnMapReadyCallback{
-
+public class MapFragment extends Fragment implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,LocationListener {
+    final String TAG = "MapFragment";
+    LocationRequest locationRequest;
+    GoogleApiClient mAppClient;
     MapView mMapView;
     GoogleMap mGoogleMap;
+    Location location;
+    Double curLat;
+    Double curLong;
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 69;
 
 
     public MapFragment() {
@@ -32,23 +68,160 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Context context = getContext();
+        LocationManager lm = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        mAppClient = new GoogleApiClient.Builder(context).addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).build();
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch(Exception ex) {}
+
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch(Exception ex) {}
+
+        if(!gps_enabled && !network_enabled) {
+            // notify user
+            AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+            dialog.setMessage("No GPS or Internet Connection");
+            dialog.setPositiveButton("Open", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+                    Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(myIntent);
+                    //get gps
+                }
+            });
+            dialog.show();
+        }
         View v = inflater.inflate(R.layout.fragment_map, container, false);
 
         mMapView = (MapView) v.findViewById(R.id.mapview);
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this); //this is important
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
 
+        curLat = location.getLatitude();
+        curLong = location.getLongitude();
         return v;
+    }
+
+    private void setUpMap(final String strUrl) {
+        // Retrieve the city data from the web service
+        // In a worker thread since it's a network operation.
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    retrieveAndAddCities(strUrl);
+                } catch (IOException e) {
+
+                }
+            }
+        }).start();
+    }
+
+    protected void retrieveAndAddCities(String strUrl) throws IOException {
+        HttpURLConnection conn = null;
+        final StringBuilder json = new StringBuilder();
+        try {
+            // Connect to the web service
+            URL url = new URL(strUrl);
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+            // Read the JSON data into the StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                json.append(buff, 0, read);
+            }
+        } catch (IOException e) {
+
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        // Create markers for the city data.
+        // Must run this on the UI thread since it's a UI operation.
+                try {
+                    createMarkersFromJson(json.toString());
+                } catch (JSONException e) {
+
+                }
+    }
+
+    void createMarkersFromJson(String json) throws JSONException {
+        // De-serialize the JSON string into an array of city objects
+        JSONArray jsonArray = new JSONArray(json);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            // Create a marker for each city in the JSON data.
+            JSONObject jsonObj = jsonArray.getJSONObject(i);
+            mGoogleMap.addMarker(new MarkerOptions()
+                    .title(jsonObj.getString("name"))
+                    .position(new LatLng(
+                            jsonObj.getJSONArray("latlng").getDouble(0),
+                            jsonObj.getJSONArray("latlng").getDouble(1)
+                    ))
+            );
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+    }
 
-        LatLng ustPosition = new LatLng(14.609676, 120.989658);
-        mGoogleMap = googleMap;
-        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
-        mGoogleMap.addMarker(new MarkerOptions().position(ustPosition));
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ustPosition, 10));
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Asking user if explanation is needed
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
@@ -79,6 +252,49 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAppClient.connect();
+    }
+    @Override
+    public void onStop(){
+        mAppClient.disconnect();
+        super.onStop();
+    }
+    @Override
+    public void onConnectionSuspended(int i){
+        Log.i(TAG, "Connection suspended");
+
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult){
+        Log.i(TAG, "connection failed");
+
+    }
+    @Override
+    public void onConnected(Bundle bundle){
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(1000);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mAppClient, locationRequest, this);
+    }
+    @Override
+    public void onLocationChanged(Location location){
+        curLat = location.getLatitude();//current latitude
+        curLong = location.getLongitude();//current longitude
+        LatLng curPos = new LatLng(curLat, curLong);
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curPos, 15));
+        StringBuilder googlePlacesUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlacesUrl.append("location=" + curLat + "," + curLong);
+        googlePlacesUrl.append("&rankby=distance");
+        googlePlacesUrl.append("&type=gas_station");
+        googlePlacesUrl.append("&sensor=true");
+        googlePlacesUrl.append("&key=AIzaSyCpxm09Wptnl10RwXdzV9FYxuiwXwCoO5E");
+        final String stringUrl = googlePlacesUrl.toString();
+        setUpMap(stringUrl);
     }
 
 }
